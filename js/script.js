2ddc642b884560eb7669fbbd7a58cb6cd815daa8
jakubcirip <101516870+jakubcirip@users.noreply.github.com>
2023-02-20 08:21:00 +0100

    add

--- js/script.js
+++ js/script.js
@@ -39,6 +39,14 @@ function previous() {
 }
 
 
+function navbarFunction(){
+    var x = document.getElementById("myTopnav");
+    if (x.className === "topnav") {
+      x.className += " responsive";
+    } else {
+      x.className = "topnav";
+    }
+}
 
 
 
