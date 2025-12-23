361085bd8d770eb1915dcceeecda3cf2aac238bb
Jakub Cirip <101516870+jakubcirip@users.noreply.github.com>
2025-12-23 11:57:18 +0100

    Add gallery functionality to kombo-galery.js

---
+++ blogs/js/kombo-galery.js
@@ -0,0 +1,21 @@
+
+const track = document.querySelector('.blog-gallery__track');
+const images = track.querySelectorAll('img');
+const prevBtn = document.querySelector('.blog-gallery__btn--prev');
+const nextBtn = document.querySelector('.blog-gallery__btn--next');
+
+let index = 0;
+
+function updateGallery() {
+  track.style.transform = `translateX(-${index * 100}%)`;
+}
+
+nextBtn.addEventListener('click', () => {
+  index = (index + 1) % images.length;
+  updateGallery();
+});
+
+prevBtn.addEventListener('click', () => {
+  index = (index - 1 + images.length) % images.length;
+  updateGallery();
+});
