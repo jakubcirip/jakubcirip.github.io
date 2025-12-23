3d8c3eb01e7c25c3befb70aa7461b3c722d8ad24
Jakub Cirip <101516870+jakubcirip@users.noreply.github.com>
2025-12-23 12:04:32 +0100

    Refactor gallery initialization and event listeners

--- blogs/js/kombo-galery.js
+++ blogs/js/kombo-galery.js
@@ -1,21 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const track = document.querySelector('.blog-gallery__track');
+    const images = track.querySelectorAll('img');
+    const prevBtn = document.querySelector('.blog-gallery__btn--prev');
+    const nextBtn = document.querySelector('.blog-gallery__btn--next');
 
-const track = document.querySelector('.blog-gallery__track');
-const images = track.querySelectorAll('img');
-const prevBtn = document.querySelector('.blog-gallery__btn--prev');
-const nextBtn = document.querySelector('.blog-gallery__btn--next');
+    let index = 0;
 
-let index = 0;
+    function updateGallery() {
+        track.style.transform = `translateX(-${index * 100}%)`;
+    }
 
-function updateGallery() {
-  track.style.transform = `translateX(-${index * 100}%)`;
-}
+    nextBtn.addEventListener('click', () => {
+        index = (index + 1) % images.length;
+        updateGallery();
+    });
 
-nextBtn.addEventListener('click', () => {
-  index = (index + 1) % images.length;
-  updateGallery();
-});
-
-prevBtn.addEventListener('click', () => {
-  index = (index - 1 + images.length) % images.length;
-  updateGallery();
+    prevBtn.addEventListener('click', () => {
+        index = (index - 1 + images.length) % images.length;
+        updateGallery();
+    });
 });
